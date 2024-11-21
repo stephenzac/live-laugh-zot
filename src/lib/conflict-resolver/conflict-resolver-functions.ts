4cf5b15c54ff5d7e9d07139f7e36f88a692ee1e0
leem07 <michelles.lee3153@gmail.com>
2024-11-21 11:21:17 -0800

    merging/adding conflict-resolver

---
+++ src/lib/conflict-resolver/conflict-resolver-functions.ts
@@ -0,0 +1,22 @@
+export const decideResolver = (
+  problem: string,
+  people: string[],
+  resolution: string
+) => {
+  const numPeople = people.length;
+  if (resolution == 'dice' && numPeople == 2) diceResolver(people);
+  else if (resolution == 'coin' && numPeople > 2) coinResolver(people);
+};
+
+export const coinResolver = (people: string[]) => {
+  const winner = Math.round(Math.random());
+  return `Do what ${people[winner]} says.`;
+};
+
+export const diceResolver = (people: string[]) => {
+  const numPeople = people.length;
+  const winner = Math.round(Math.random() * numPeople);
+  for (let i = 0; i < numPeople; i++) {
+    if (winner == i) return `${people[i]} is in the right.`;
+  }
+};
