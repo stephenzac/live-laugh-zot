693481e2110ecc86f7505aa977d56c5383ae6018
stephenzac <stephenzacarias2@gmail.com>
2024-11-22 15:43:36 -0800

    Styling

--- src/app/household/[householdId]/page.tsx
+++ src/app/household/[householdId]/page.tsx
@@ -9,14 +9,27 @@ import { Treasury } from '@/components/treasury/treasury';
 import { ConflictResolver } from '@/components/conflict-resolver/conflict-resolver';
 import { TheForum } from '@/components/the-forum/the-forum';
 
+import { Goblin_One } from 'next/font/google';
+
+const goblin = Goblin_One({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-goblin',
+  weight: ['400'],
+});
+
 export default function HouseholdPage() {
   const { name, id, authenticated } = useHouseholdStore();
 
   if (!authenticated) redirect('/');
 
   return (
-    <div className='flex flex-col gap-8 mb-8'>
-      <h1 className='text-3xl font-bold mb-14'>Welcome to {name}!</h1>
+    <div className='flex flex-col gap-8'>
+      <h1
+        className={`${goblin.className} text-3xl md:text-5xl w-full text-center md:mb-8`}
+      >
+        Welcome to {name}!
+      </h1>
 
       <div className='flex flex-col md:flex-row gap-8'>
         <div className='flex flex-col justify-center md:flex-row gap-8 w-full'>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,24 @@
 import { LoginForm } from '@/components/login-form';
 import { CreateHouseholdForm } from '../components/create-household-form';
+import { Fascinate_Inline } from 'next/font/google';
+
+const fascinate = Fascinate_Inline({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-fascinate',
+  weight: ['400'],
+});
 
 export default function Home() {
   return (
     <>
       <header className='mb-10'>
-        <h1 className='text-4xl text-gray-800 font-bold italic text-center sm:text-left'>
-          Live, Laugh, Zot
+        <h1 className={`${fascinate.className} text-3xl md:text-5xl mb-5`}>
+          Live, Laugh, Zot 🌞
         </h1>
+        <h2 className='text-xl mb-5 italic'>
+          The perfect tool for Anteater households!
+        </h2>
       </header>
 
       <div className='flex flex-col gap-8 sm:flex-row sm:gap-8 md:gap-16'>
