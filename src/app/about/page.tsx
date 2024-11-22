7c718fe47de7382a5922b315b87f39ce674071b5
stephenzac <stephenzacarias2@gmail.com>
2024-11-22 15:50:18 -0800

    Add about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,27 @@
+import { Goblin_One } from 'next/font/google';
+
+const goblin = Goblin_One({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-goblin',
+  weight: ['400'],
+});
+
 export default function About() {
   return (
     <>
-      <h1>About us</h1>
+      <header className='w-full text-center mb-8'>
+        <h1 className={`${goblin.className} text-[30px] mb-10`}>About Us</h1>
+        <h2 className='text-[25px] mb-5'>
+          We designed this tool for Anteaters living together, to make the
+          college experience more enojoyable
+        </h2>
+      </header>
+
+      <h2 className='text-[25px] mb-3'>Meet the Team:</h2>
+      <h2 className='text-[20px] mb-3'>Lead/Front-End: Stephen Zacarias</h2>
+      <h2 className='text-[20px] mb-3'>Back-End: Michelle Lee</h2>
+      <h2 className='text-[20px] mb-3'>Back-End: Yoav Feigenbaum</h2>
     </>
   );
 }
