5db9ab0d01c2605b253c07a95829d0f18347e149
Yoav Feigenbaum <yfeigenb@uci.edu>
2024-11-22 00:57:15 -0800

    minor css tweaks

--- src/components/app/footer.tsx
+++ src/components/app/footer.tsx
@@ -1,8 +1,8 @@
 import Link from 'next/link';
 
 export const Footer: React.FC = () => (
-  <div className='w-full h-24 flex flex-col justify-center items-center bg-orange-200'>
-    <h1 className='text-4xl text-gray-600 italic mb-3'>live, laugh, zot</h1>
+  <div className='w-full h-24 flex flex-col justify-center items-center bg-orange-10'>
+    <h1 className='text-2xl text-grey-600 italic mb-3'>Live, Laugh, Zot</h1>
     <Link href='/about'>
       <span className='italic'>About Us</span>
     </Link>
