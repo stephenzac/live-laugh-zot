af45ce7ef3e29b1b551b65928e17d26121128277
leem07 <michelles.lee3153@gmail.com>
2024-11-21 19:00:08 -0800

    decide-resolver-ai-api

--- src/lib/conflict-resolver/conflict-resolver-functions.ts
+++ src/lib/conflict-resolver/conflict-resolver-functions.ts
@@ -1,10 +1,23 @@
+import OpenAI from 'openai';
+
+const baseURL = 'https://api.aimlapi.com/v1';
+const apiKey = process.env.OPENAI_KEY;
+
+const api = new OpenAI({ apiKey, baseURL });
+
 export const decideResolver = (
   problem: string,
   people: string[],
   resolution: string
 ) => {
-  if (resolution == 'dice') diceResolver(people);
-  else if (resolution == 'coin') coinResolver(people);
+  const numPeople = people.length;
+  if (resolution == 'dice' && numPeople == 2) return diceResolver(people);
+  else if (resolution == 'coin' && numPeople > 2) return coinResolver(people);
+  else if (resolution == 'AI') {
+    const systemPrompt = `You are resolving a conflict. The person(s) involved are: 
+      ${people.join(', ')}. Be descriptive and give a decisive solution.`;
+    return decideAI(problem, systemPrompt);
+  }
 };
 
 export const coinResolver = (people: string[]) => {
@@ -18,4 +31,26 @@ export const diceResolver = (people: string[]) => {
   for (let i = 0; i < numPeople; i++) {
     if (winner == i) return `${people[i]} is in the right.`;
   }
+};
+
+export const decideAI = async (userInput: string, systemPrompt: string) => {
+  const completion = await api.chat.completions.create({
+    model: 'mistralai/Mistral-7B-Instruct-v0.2',
+    messages: [
+      {
+        role: 'system',
+        content: systemPrompt,
+      },
+      {
+        role: 'user',
+        content: userInput,
+      },
+    ],
+    temperature: 0.7,
+    max_tokens: 256,
+  });
+
+  const chatResponse = completion.choices[0].message.content;
+
+  return chatResponse;
 };
